9af4fd99e9c4a98c2aae3540cc679c2890a7d566
RomanTutskyi <romachikkj@gmail.com>
2022-11-21 11:08:26 +0200

    Update class.py

--- class.py
+++ class.py
@@ -1,10 +1,17 @@
 class Pet:
-    def __init__(self , name ,weight , color ):
+    def __init__(self , name ,weight , color , your_name):
         self.name = name 
         self.weight = weight 
         self.color = color 
+        self.your_name = your_name
         
     def say_hello(self):
         return f'His name is {self.name} , weight is {self.weight} , and his color is {self.color}'
-#p = Pet('ivan', '35', 'red')
-#print(p.say_hello())
+
+class Person(Pet):
+    def name(self):
+        return f'Your name is {self.your_name} '
+p = Pet('ivan', '35', 'red' 'mukola')
+print(p.say_hello())
+m = Pet()
+print(m.name())
